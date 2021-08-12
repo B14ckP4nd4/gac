c6b7230016b5c1db9863805d503dfb4ad5413b1a
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-08-12 00:48:04 -0700

    fix service name typo

--- install.sh
+++ install.sh
@@ -4,13 +4,9 @@
 # present working directory
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo $script_path
-
 # copy scripts and servicec
 \cp -rf "${script_path}/src/gac.service" /lib/systemd/system/
 \cp -rf "${script_path}/src/gac.sh" /usr/local/bin/
-
-echo "${script_path}/src/gac.service"
 
 # set permissions
 chmod 664 /lib/systemd/system/gac.service
@@ -18,4 +14,4 @@ chmod +x /usr/local/bin/gac.sh
 
 # reload system controller
 systemctl daemon-reload
-systemctl enable test.service
+systemctl enable gac.service
