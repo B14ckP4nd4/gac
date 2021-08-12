4af1b772bf297871c9e70523fa44356defcd4e04
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-08-12 00:46:52 -0700

    test path

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-set -x
 
 # present working directory
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+echo $script_path
+
 # copy scripts and servicec
 \cp -rf "${script_path}/src/gac.service" /lib/systemd/system/
 \cp -rf "${script_path}/src/gac.sh" /usr/local/bin/
+
+echo "${script_path}/src/gac.service"
 
 # set permissions
 chmod 664 /lib/systemd/system/gac.service
@@ -16,7 +19,3 @@ chmod +x /usr/local/bin/gac.sh
 # reload system controller
 systemctl daemon-reload
 systemctl enable test.service
-
-
-
-set +x
