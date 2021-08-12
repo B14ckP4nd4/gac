90581206bf3d25bdfc482b3e8cbd899737680c17
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-08-12 00:35:31 -0700

    create source and installition script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo $script_path
---
+++ src/gac.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source /etc/gac/config.sh
+
+while true; do
+
+# wait 5 Sec
+sleep 5;
+
+done;
