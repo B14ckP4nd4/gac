2a7d8c9f5cfe191de1f43aa4df8b49eea75e7423
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-08-16 01:57:47 -0700

    change if statement

--- src/gac.sh
+++ src/gac.sh
@@ -14,7 +14,15 @@ while true; do
 
     wget -q $auto_running_script -O /tmp/gac_last_update
 
-    cmp --silent $command_path /tmp/gac_last_update || \cp -rf /tmp/gac_last_update /usr/local/bin/ $command_path && rm -rf /etc/gac/running
+    #cmp --silent $command_path /tmp/gac_last_update || \cp -rf /tmp/gac_last_update /usr/local/bin/ $command_path && rm -rf /etc/gac/running
+
+    if ! cmp $command_path /tmp/gac_last_update > /dev/null 2>&1
+    then
+        rm -rf $command_path
+        yes | mv /tmp/gac_last_update $command_path
+        rm -rf /etc/gac/running
+    fi
+
 
 
 done;
